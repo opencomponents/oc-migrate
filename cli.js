8e218e3ed74d50847657ddcf157300e0e4fcf581
Matteo Figus <matteofigus@gmail.com>
2016-07-06 17:50:12 +0100

    migration path for 0.33.X

--- cli.js
+++ cli.js
@@ -6,12 +6,18 @@ const async = require('async');
 const colors = require('colors/safe');
 const request = require('minimal-request');
 const semver = require('semver');
+const _ = require('lodash');
 
-const getOcLatestVersion = (cb) => {
+var pkg = require('./package');
+
+const getLatestVersion = (module, cb) => {
   request({
-    url: 'https://skimdb.npmjs.com/registry/_design/app/_view/byField?key=%22oc%22',
+    url: `https://skimdb.npmjs.com/registry/_design/app/_view/byField?key=%22${module}%22`,
     json: true
-  }, (err, response) => err ? cb(`Unable to connect to NPM: ${err}`) : cb(err, response.rows[0].value.version));
+  }, (err, response) => {
+    if(err){ return cb(`Unable to connect to NPM: ${err}`); }
+    cb(null, response.rows[0].value.version);
+  });
 };
 
 const getParams = (cb) => {
@@ -46,10 +52,13 @@ const getHandlebars3Components = (registryUrl, cb) => {
         json: true
       }, (err, componentInfo) => {
 
-        const isHandlebars = componentInfo.oc.files.template.type === 'handlebars',
-              isHandlebars3 = isHandlebars && (!componentInfo.oc.version || semver.lt(componentInfo.oc.version, '0.32.0'));
+        if(err){ console.log(err); console.log(componentInfo); return next(err); }
 
-        if(isHandlebars3){
+        const isHandlebars = componentInfo.oc.files.template.type === 'handlebars',
+              isHandlebars3 = isHandlebars && (!componentInfo.oc.version || semver.lt(componentInfo.oc.version, '0.32.0')),
+              isDeprecated = componentInfo.oc.state === 'deprecated';
+
+        if(isHandlebars3 && !isDeprecated){
           h3components.push(componentInfo);
         }
         
@@ -72,35 +81,59 @@ getParams((err, registryUrl) => {
   if(err){ return exit(err); }
 
   async.parallel({
-    ocVersion: next => getOcLatestVersion(next),
+    ocMigrateVersion: next => getLatestVersion('oc-migrate', next),
+    ocVersion: next => getLatestVersion('oc', next),
     registryVersion: next => getRegistryVersion(registryUrl, next)
   }, (err, results) => {
 
     if(err){ return exit(err); }
 
-    if(results.ocVersion === results.registryVersion){
-      console.log(colors.green('Registry is already using latest version'));
+    if(semver.lt(pkg.version, results.ocMigrateVersion)){
+      exit(`oc-migrate is outdated. For upgrading run: [sudo] npm i -g oc-migrate`);
+    }
+
+    console.log(`Latest OC version: ${colors.green(results.ocVersion)}`);
+    console.log(`${registryUrl} version: ${colors.green(results.registryVersion)}`);
+    
+    const diff = semver.diff(results.ocVersion, results.registryVersion);
+    const safeUpgrade = (diff === 'patch') || (semver.major(results.ocVersion) > 0 && diff === 'minor');
+
+    if(!diff){
+      console.log(colors.green('${registryUrl} is already using latest version. Well done.'));
+    } else if(safeUpgrade){
+      console.log(colors.green(`${registryUrl} can be safely upgraded from ${results.registryVersion} to ${results.ocVersion}.`));
     } else {
+      console.log(colors.yellow(`${registryUrl} should be upgraded to the latest version, but be careful: breaking changes are listed here: https://github.com/opentable/oc/blob/master/CHANGELOG.md`));
+    }
 
-      if(semver.lt(results.registryVersion, '0.32.0')){
+    if(semver.lt(results.registryVersion, '0.33.0')){
 
-        getHandlebars3Components(registryUrl, (err, components) => {
-          if(components.length === 0){
-            console.log(colors.green(`You can safely upgrade from ${results.registryVersion} to ${results.ocVersion}`));
-          } else {
-            console.log(colors.yellow(`You need to upgrade OC to 0.32.X, which supports both Handlebars 3 and 4 so that you can gracefully upgrade your components.`));
-            console.log(colors.yellow(`Then, the following components will need to be re-published using Handlebars 4. After that, you will be able to re-run this tool for upgrading to a more recent version.`));
+      console.log(colors.yellow(`Analysing components...`));
 
-            _.each(components, (component) => {
-              console.log(colors.red(`${component.name}@${component.version} - Maintained by ${component.author || 'unknown'} ${component.repository || ''}`));
-            });
-          }
+      getHandlebars3Components(registryUrl, (err, components) => {
 
-          exit();
-        });
-      } else {
+        if(err){ return exit(err); }
+        
+        if(components.length > 0){
+
+          console.log(colors.yellow(`Warning: OC v0.33.X removes support for Handlebars 3, and it looks like some of your components will break if you upgrade to >0.33.X.`))
+          console.log(colors.yellow(`${registryUrl} will need to be upgraded to 0.32.X, which supports both Handlebars 3 and 4.`));
+          console.log(colors.yellow(`Then, the following components will need to be re-published using Handlebars 4. After that, re-run this tool for upgrading ${registryUrl} to a more recent version.`));
+          console.log(colors.yellow(`Note: given OC components immutability, after publishing a new version of each component, you will need to ensure no consumer is consuming previous versions`));
+
+          _.each(components, (component, i) => {
+            const author = _.isObject(component.author) ? JSON.stringify(component.author) : component.author;
+            const repository = _.isObject(component.repository) ? JSON.stringify(component.repository) : component.repository;
+            console.log(colors.red(`${i + 1})\t${component.name}@${component.version} - Maintained by ${author || 'unknown'} ${repository || ''}`));
+          });
+        } else {
+          console.log(colors.green('OK'));
+        }
+
         exit();
-      }
+      });
+    } else {
+      exit();
     }
   });
 });
